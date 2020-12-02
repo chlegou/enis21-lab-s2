928dee730be7f5c006c7da5d932c2045fc1e00e8
Houcem Ben Amor Chlegou <nicolastsue@gmail.com>
2020-12-02 10:01:11 +0100

    create member form component. add basic routing.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { MemberListComponent } from './member-list/member-list.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MaterialModule} from './material.module';
+import { MemberFormComponent } from './member-form/member-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MemberListComponent,
+    MemberFormComponent,
   ],
   imports: [
     BrowserModule,
